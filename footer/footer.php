07c862e9001dbd0114e3312eeee9a906d68d865f
Esmeralda1217 <silvacoxcahua@gmail.com>
2022-02-10 10:55:28 -0600

    modifique index y footer

---
+++ footer/footer.php
@@ -0,0 +1,20 @@
+
+<html>
+    <meta charset="utf-8">
+    <title> ejemplo de un footer </title>
+    <header>Diseño de un pie de pagina</head>
+    <body>
+
+         <h1>The Bloody Mary</h1>
+<p>To make a perfect Boody Mary you definitely need to get your hans dirty...</p>
+<footer>
+  <address>
+    Jhonny Doe<br>
+    jhonnydoe5468@mypc.com
+  </address>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/food">Food</a></li>
+    <li><a href="/drinks">Drinks</a></li>
+  </ul>
+</footer>
