3b99d7cf574b6719ac19d4b1ac0dcfdb6f075742
joseluissanchezrivera <ingti.joseluissanchezrivera@gmail.com>
2022-02-11 12:43:23 -0600

    cambio de nombre carpeta de css

---
+++ card/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Profile Card Design</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="profile-card">
+    <div class="image-container">
+
+    </div>
+
+    </div>
+</body>
+</html>
