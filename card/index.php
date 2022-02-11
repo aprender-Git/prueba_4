f0aa5456c83e20c4bd4a4f98f7e218f6599e459e
joseluissanchezrivera <ingti.joseluissanchezrivera@gmail.com>
2022-02-11 15:44:46 -0600

    card

--- card/index.php
+++ card/index.php
@@ -1,18 +1,46 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <title>Profile Card Design</title>
-    <link rel="stylesheet" href="css\style.css">
-    <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imagenes con Fetch</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/vanillatoasts.css">
 </head>
+
 <body>
-    <div class="profile-card">
-    <div class="image-container">
-        <img src="img\perro.png" style="width: 100%">
-        <div class="title ">
-            <h2>Rocky</h2>
+    <div class="content">
+        <label for="img" class="button"></label>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 m-auto">
+                <div class="card d-none" id="vistaPrevia">
+                    <div class="card-body">
+                        <form method="post" action="" id="frmImg">
+                            <input type="file" name="imagen" id="img" class="d-none">
+                            <img src="" alt="" id="imgprevia" class="img-thumbnail">
+                            <div class="form-group">
+                                <label for="comentario">Comentario</label>
+                                <textarea id="comentario" class="form-control" name="comentario" rows="3"></textarea>
+                            </div>
+                            <button class="btn btn-primary" type="button" id="btn-publicar">Publicar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+    <div class="container">
+        <h4 class="text-center text-white">Publicaciones</h4>
+        <div class="row" id="resultado">
 
+        </div>
     </div>
+    <script src="js/vanillatoasts.js"></script>
+    <script src="js/script.js"></script>
 </body>
+
 </html>
---
+++ card2/index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <title>Profile Card Design</title>
+    <link rel="stylesheet" href="css\style.css">
+    <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="profile-card">
+    <div class="image-container">
+        <img src="img\perro.png" style="width: 100%">
+        <div class="title ">
+            <h2>Rocky</h2>
+        </div>
+    </div>
+    <div>
+
+
+    </div class="main-container">
+
+    <p><i class="fa-solid fa-dog"></i>Raza aparente</p>
+    <p><i class="fa-solid fa-ruler-vertical"></i> Talla</p>
+    <p><i class="fa-solid fa-hourglass"></i> Edad</p>
+    <p><i class="fa fa-venus-mars" aria-hidden="true"></i>
+        </i> Sexo</p>
+    <p><i class="fa fa-users" aria-hidden="true"></i>
+        Sociable con</p>
+    <p><br></br></p>
+    </div>
+</body>
+</html>
