867b90c3b3d84b709538c597235092d9cce169af
Esmeralda1217 <silvacoxcahua@gmail.com>
2022-03-02 17:02:47 -0600

    botones

---
+++ vistas/botones/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="estilo.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></head>
+  <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <body>
+
+<div class="container">
+<h2>Button Styles</h2>
+
+<div class="row">
+                <div class="col-6   texto">
+                    <button type="button" class="btn btn-success botones">
+                        <i class='fas fa-dog' style='font-size:16px'></i>
+                    Adoptar
+                </button>
+                </div>
+
+                <div class="col-6   texto2">
+                <button type="button" class="btn btn-info botones">
+                    
+                <i class='fas fa-paw' style='font-size:16px'></i> Ver mas
+                </button>
+                <br>
+</div>
+
+</div>
+</body>
+</html>
